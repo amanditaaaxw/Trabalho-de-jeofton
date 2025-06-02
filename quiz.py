e3e35a4bfaefd38fe7c6db86e1ed06ac54d64b1a
Amanda <amandaclira07@gmail.com>
2025-06-02 08:50:39 -0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,266 @@
+print("Bem-vindo ao quiz sobre a cultura das regiões do Brasil!")
+print("Responda com a letra da alternativa correta (a, b, c ou d).\n")
+input("tecle enter para começar")
+pontos = 0
+
+
+print("1) Quais são as três cidades-capitais da região Sul?\n")
+print("a) Porto Alegre, Florianópolis e Curitiba")
+print("b) Curitiba, Joinville e Caxias do Sul")
+print("c) Porto Alegre, Brasília e Santa Maria")
+print("d) Florianópolis, Rio de Janeiro e São Paulo")
+resposta = input("Resposta: ")
+if resposta == "a":
+    print("resposta correta\n")
+    pontos = pontos + 1
+else:
+    print("resposta incorreta\n")
+
+
+print("2) Qual é o prato típico do Rio Grande do Sul?\n")
+print("a) Feijoada")
+print("b) Arroz carreteiro")
+print("c) Moqueca")
+print("d) Galinhada")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("3) Qual estado tem as Cataratas do Iguaçu?\n")
+print("a) Santa Catarina")
+print("b) Paraná")
+print("c) Rio Grande do Sul")
+print("d) São Paulo")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("4) O que caracteriza o clima da região Sul?\n")
+print("a) Tropical e seco")
+print("b) Equatorial e úmido")
+print("c) Temperado e com estações bem definidas")
+print("d) Ártico e gelado")
+resposta = input("Resposta: ")
+if resposta == "c":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+print("5) Qual é o principal bioma do Centro-Oeste?\n")
+print("a) Amazônia")
+print("b) Caatinga")
+print("c) Cerrado")
+print("d) Pantanal")
+resposta = input("Resposta: ")
+if resposta == "c":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("6) Qual famoso monumento está em Brasília?\n")
+print("a) Cristo Redentor")
+print("b) Palácio da Alvorada")
+print("c) Teatro Amazonas")
+print("d) Catedral da Sé")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("7) Qual a principal atividade econômica do Centro-Oeste?\n")
+print("a) Indústria têxtil")
+print("b) Agropecuária")
+print("c) Turismo ecológico")
+print("d) Tecnologia da informação")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("8) Qual cidade é a 'capital do agronegócio'?\n")
+print("a) Goiânia")
+print("b) Campo Grande")
+print("c) Brasília")
+print("d) Cuiabá")
+resposta = input("Resposta: ")
+if resposta == "d":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("9) Qual é a capital do estado da Bahia?\n")
+print("a) Recife")
+print("b) Salvador")
+print("c) Fortaleza")
+print("d) São Luiz")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("10) Qual ritmo musical é típico das festas juninas?\n")
+print("a) Samba")
+print("b) Forró")
+print("c) Axé")
+print("d) Funk")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("11) Qual estado NÃO faz parte do Nordeste?\n")
+print("a) Maranhão")
+print("b) Goiás")
+print("c) Piauí")
+print("d) Rio Grande do Norte")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("12) Qual parque fica na Chapada Diamantina?\n")
+print("a) Parque Nacional da Serra da Capivara")
+print("b) Parque Nacional de Jericoacoara")
+print("c) Parque Nacional da Chapada Diamantina")
+print("d) nenhuma das alternativas")
+resposta = input("Resposta: ")
+if resposta == "c":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("13) Qual é o maior estado da Região Norte?\n")
+print("a) Amazonas")
+print("b) Acre")
+print("c) Roraima")
+print("d) Pará")
+resposta = input("Resposta: ")
+if resposta == "a":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("14) Qual animal é símbolo da fauna amazônica?\n")
+print("a) Onça-pintada")
+print("b) Lobo-guará")
+print("c) Tamanduá-bandeira")
+print("d) Arara-azul")
+resposta = input("Resposta: ")
+if resposta == "a":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("15) Qual é o principal bioma da Região Norte?\n")
+print("a) Cerrado")
+print("b) Caatinga")
+print("c) Amazônia")
+print("d) Pantanal")
+resposta = input("Resposta: ")
+if resposta == "c":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("16) Onde fica o Encontro das Águas?\n")
+print("a) Belém")
+print("b) Manaus")
+print("c) Porto Velho")
+print("d) Boa Vista")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("17) Qual é o estado mais populoso do Sudeste?\n")
+print("a) Minas Gerais")
+print("b) São Paulo")
+print("c) Rio de Janeiro")
+print("d) Espírito Santo")
+resposta = input("Resposta: ")
+if resposta == "b":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("18) Qual cidade é chamada de 'Cidade Maravilhosa'?\n")
+print("a) São Paulo")
+print("b) Belo Horizonte")
+print("c) Rio de Janeiro")
+print("d) Vitória")
+resposta = input("Resposta: ")
+if resposta == "c":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("19) Qual é a capital do Espírito Santo?\n")
+print("a) Vitória")
+print("b) Belo Horizonte")
+print("c) Niterói")
+print("d) Campinas")
+resposta = input("Resposta: ")
+if resposta == "a":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+
+print("20) Onde está o Cristo Redentor?\n")
+print("a) Brasília")
+print("b) São Paulo")
+print("c) Rio de Janeiro")
+print("d) Belo Horizonte")
+resposta = input("Resposta: ")
+if resposta == "c":
+    pontos = pontos + 1
+    print("resposta correta\n")
+else:
+    print("resposta incorreta\n")
+
+print()
+print("Você acertou", pontos, "de 20 perguntas.")
